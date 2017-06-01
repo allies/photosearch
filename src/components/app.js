3b3dc4ddadf7308607222c42f5cb6bc4b9fd8453
Rohit Motwani <rohittmotwani@gmail.com>
2017-06-01 13:59:11 +0530

    minor changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './header';
-import Footer from './footer';
 import Searchbox from './searchbox';
+import Footer from './footer';
 
 
 class App extends React.Component {
